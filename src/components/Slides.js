90f02fa08ebe667b552629ff6a90a89d8418b104
Gabriel Braz <gabriel.freire.braz@avanade.com>
2024-07-28 21:50:25 -0300

    all

--- src/components/Slides.js
+++ src/components/Slides.js
@@ -1,21 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+
+
+
+
+
 
 function Slides({slides}) {
+    const [activeBanner, setActiveBanner] = useState(slides[0] ?? {});
 
+    function onRestart() {
+        setActiveBanner(slides[0])
+    }
+    
+    
+    function onClick(type) {
+        const indexActive = slides.findIndex((currValCheck) => currValCheck === activeBanner)
+
+        setActiveBanner(() => type === 'next' ? slides[indexActive+1] : slides[indexActive-1])
+    }
+    
     return (
         <div>
             <div id="navigation" className="text-center">
-                <button data-testid="button-restart" className="small outlined">Restart</button>
-                <button data-testid="button-prev" className="small">Prev</button>
-                <button data-testid="button-next" className="small">Next</button>
+                <button data-testid="button-restart" className="small outlined" onClick={onRestart}>Restart</button>
+                <button data-testid="button-prev" className="small" onClick={() => onClick('prev')}>Prev</button>
+                <button data-testid="button-next" className="small" onClick={() => onClick('next')}>Next</button>
             </div>
-            <div id="slide" className="card text-center">
-                <h1 data-testid="title">Slide Title Here</h1>
-                <p data-testid="text">Slide Text Here</p>
-            </div>
+
+            <Slide content={activeBanner} />
         </div>
     );
+}
 
+function Slide({content} = content) {
+    return content && (
+        <div id="slide" className="card text-center">
+            <h1 data-testid="title">{content.title}</h1>
+            <p data-testid="text">{content.text}</p>
+        </div>
+    )
 }
 
 export default Slides;
