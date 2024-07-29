6ea02b871d5b406fe8b043f3055c66dcd6f4dae3
Gabriel Braz <gabriel.freire.braz@avanade.com>
2024-07-28 23:37:40 -0300

    all 2

--- src/components/Slides.js
+++ src/components/Slides.js
@@ -1,31 +1,50 @@
-import React, { useState } from 'react';
-
-
-
-
-
+import React, { useEffect, useState } from 'react';
 
 
 function Slides({slides}) {
     const [activeBanner, setActiveBanner] = useState(slides[0] ?? {});
 
+    const [prevDisabled, setPrevDisabled] = useState(false);
+    const [nextDisabled, setNextDisabled] = useState(false);
+
+
+    useEffect(() => {
+        const indexActive = currentIndexBanner()
+        const total_slides = slides.length-1
+   
+        setPrevDisabled(indexActive === 0)
+        setNextDisabled(indexActive === total_slides)
+
+    }, [activeBanner])
+
+
     function onRestart() {
         setActiveBanner(slides[0])
     }
     
+    function currentIndexBanner() {
+        return slides.findIndex((currValCheck) => currValCheck === activeBanner)
+    }
     
     function onClick(type) {
-        const indexActive = slides.findIndex((currValCheck) => currValCheck === activeBanner)
+        const indexActive = currentIndexBanner()
 
-        setActiveBanner(() => type === 'next' ? slides[indexActive+1] : slides[indexActive-1])
+        if (indexActive > -1) {
+            const nextSlide = slides[indexActive+1]
+            const prevSlide = slides[indexActive-1]
+            
+            
+            if (type === 'next' && nextSlide) setActiveBanner(() => nextSlide)
+            else if (type === 'prev' && prevSlide) setActiveBanner(() => prevSlide)
+        } 
     }
     
     return (
         <div>
             <div id="navigation" className="text-center">
                 <button data-testid="button-restart" className="small outlined" onClick={onRestart}>Restart</button>
-                <button data-testid="button-prev" className="small" onClick={() => onClick('prev')}>Prev</button>
-                <button data-testid="button-next" className="small" onClick={() => onClick('next')}>Next</button>
+                <button data-testid="button-prev" className="small" onClick={() => onClick('prev')} disabled={prevDisabled}>Prev</button>
+                <button data-testid="button-next" className="small" onClick={() => onClick('next')} disabled={nextDisabled}>Next</button>
             </div>
 
             <Slide content={activeBanner} />
